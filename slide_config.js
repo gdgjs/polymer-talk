cfc0b04eeccd34508320b26326f2eea8259d50ed
Lucas Churchill <lucas@agtlucas.com>
2014-05-26 23:03:19 -0300

    Update slide_config

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Polymer',
+    subtitle: '...',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
@@ -19,12 +19,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Lucas da Silva',
+    company: 'Front-End Ops, Alkaw',
+    gplus: 'http://plus.google.com/+LucasChurchill',
+    twitter: '@_agtlucas',
+    www: 'http://agtlucas.com',
+    github: 'http://github.com/agtlucas'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
