f77be37ad031c6fb96da5d68dba6a1aab784c0ba
Lucas Churchill <lucas@agtlucas.com>
2014-05-30 11:16:58 -0300

    Adding Addy Osmani slide's fork

---
+++ README.md
@@ -0,0 +1,7 @@
+Fork da apresentação do Addy Osmani
+==========
+
+FITC Web Components Can Do That?!
+
+[Presentation](http://addyosmani.github.io/fitc-wccdt/)
+
---
+++ slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Polymer',
+    subtitle: "O futuro é agora",
+    eventInfo: {
+      title: 'GDG Jaraguá do Sul',
+      date: 'May 31th, 2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: false, // Controlled in app.js
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: '/images/p-logo-16.png',
+    fonts: [
+      'Open Sans:600italic,400,300,600',
+      'Source Code Pro',
+      'Architects Daughter'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Lucas da Silva',
+    gplus: 'http://google.com/+LucasChurchill',
+    twitter: '@_agtlucas',
+    www: 'http://lucas.ninja',
+    github: 'http://github.com/agtlucas'
+  }]
+};
+
